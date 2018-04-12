55735efb3ef6c9fd39c94f5ed7e5de275072544f
dreyna <reyna_b@upeu.edu.pe>
2018-04-12 18:21:18 -0500

    dfghjk

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+var padre = document.getElementById("padre");
+var nod = document.createElement("li");
+var texto = document.createTextNode("cuatro");
+nod.appendChild(texto);
+padre.appendChild(nod);
+*/
+function mensaje(){
+	var x = document.getElementById("nombre").value;
+	document.getElementById("m").value= x;
+}
